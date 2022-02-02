3e31bfeac389669937c235a9167b2a83372f5734
Marcellous Curtis <1mcj001@gmail.com>
2022-02-01 23:57:39 -0600

    Jasmine isnt working

--- servers.test.js
+++ servers.test.js
@@ -7,27 +7,32 @@ describe("Servers test (with setup and tear-down)", function () {
   it("should add a new server to allServers on submitServerInfo()", function () {
     submitServerInfo();
 
-    expect(Object.keys(allServers).length).toEqual(1)
-    expect(allServers['server' + serverId].serverName).toEqual('Alice')
-
+    expect(Object.keys(allServers).length).toEqual(1);
+    expect(allServers["server" + serverId].serverName).toEqual("Alice");
   });
 
-  it("should not add name is input value is empty", function () {
+  it("should not add name if input value is empty", function () {
     submitServerInfo();
+    serverNameInput.value = "";
 
-
+    expect(Object.keys(allServers).length).toEqual(0);
   });
 
-  it("", function () {
-    
+  it("should update server table", function () {
+    submitServerInfo();
+    updateServerTable();
 
+    let curTdList = document.querySelectorAll("#serverTable tbody tr td");
 
+    expect(curTdList.length).toEqual(2);
+    expect(curTdList[0].innerText).toEqual("Alice");
+    expect(curTdList[1].innerText).toEqual("$0.00");
   });
-
 
   afterEach(function () {
-    severId =0;
-    serverTbody.innerHTML
+    severId = 0;
+    serverTbody.innerHTML;
+    allServers = {};
     // teardown logic
   });
 });
